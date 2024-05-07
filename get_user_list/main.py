1f1fec54b6a8bea7df42d20e01a68ab1dfe18085
Pierre-Jordi Guettaf <pierrejordiguettaf@gmail.com>
2024-05-07 09:35:23 +0200

    Création get list pour frontend

---
+++ get_user_list/main.py
@@ -0,0 +1,30 @@
+"""Fonction pour récupérer la liste des id des user"""
+from io import BytesIO
+import pandas as pd
+import flask
+import functions_framework
+from google.cloud import storage
+
+client_gcs = storage.Client(project='p9-reco-contenu')
+bucket = client_gcs.get_bucket('data-p9-reco')
+
+
+def get_list():
+    '''Précharge le jeu de données et récupère la liste comme variable
+    d'instance (réduit le temps de latence pour les démarrages à froid).'''
+    train_raw = BytesIO(bucket.get_blob(
+        'train.feather').download_as_bytes())
+    liste = pd.read_feather(BytesIO(train_raw)).user_id.unique()
+
+    return liste
+
+
+user_list = get_list()
+
+
+@functions_framework.http
+def get_user_list(request: flask.Request) -> flask.typing.ResponseReturnValue:
+    '''
+    Renvoie la liste des ids de users contenue dant train.feather
+    '''
+    return user_list
